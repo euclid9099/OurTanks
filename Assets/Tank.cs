7d36816d8ecc61d6f49e9d895445f25a333e16c3
euclid9099 <87277722+euclid9099@users.noreply.github.com>
2022-02-15 11:49:49 +0100

    Added collision detection

--- Assets/Tank.cs
+++ Assets/Tank.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class Tank : MonoBehaviour
 {
     private float speed = 10f;
@@ -16,18 +17,52 @@ public class Tank : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //get Input for horizontal (a,d,LEFT,RIGHT) and vertical (w,s,UP,DOWN) axis
         movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
 
-        //set rotation based on movement
-        transform.eulerAngles = new Vector3(0,0, (float)(180 * Math.Atan2(movement.y, movement.x) / Math.PI));
-
         //if movement is not null, move player
-        if(!movement.Equals(Vector3.zero))
+        if (!movement.Equals(Vector3.zero))
         {
-            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+            //change speed to always be the same (strafing provides no benefits)
+            movement.Normalize();
+            movement *= speed * Time.deltaTime;
+
+            //slowly rotate to fit movement
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.Euler(0,0,(float)(180 * Math.Atan2(movement.y, movement.x) / Math.PI)), Time.deltaTime * 25);
+
+            //collision detection:
+            //find all collidable objects within movement range
+            RaycastHit2D[] hits = Physics2D.BoxCastAll(transform.position, myCollider.size, 0, movement, speed * Time.deltaTime, LayerMask.GetMask("Tank", "Obstruction"));
+
+            //iterate over each hit
+            foreach (RaycastHit2D hit in hits)
+            {
+                //first element is self, ignore
+                if(!hit.Equals(hits[0]))
+                {
+                    //check x value of hits normal
+                    if (hit.normal.x != 0)
+                    {
+                        //if there is a sign mismatch (e.g. hit.normal.x = -1 and movement.x = 1), there would be a collision. Decrease speed
+                        if ((hit.normal.x < 0 && movement.x > 0) || (hit.normal.x > 0 && movement.x < 0))
+                        {
+                            movement.x *= hit.distance;
+                        }
+                    }
+                    //same for y
+                    else if (hit.normal.y != 0)
+                    {
+                        if ((hit.normal.y < 0 && movement.y > 0) || (hit.normal.y > 0 && movement.y < 0))
+                        {
+                            movement.y *= hit.distance;
+                        }
+                    }
+                }
+            }
+            //translate tank by movement, after every collision has taken effect
+            transform.Translate(movement, Space.World);
         }
     }
 }
