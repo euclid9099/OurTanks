f777a90cf736b0b3374afc492bcc89273525bafd
euclid9099 <87277722+euclid9099@users.noreply.github.com>
2022-03-11 12:50:25 +0100

    moved icon loading to new function

--- Assets/MapLoader.cs
+++ Assets/MapLoader.cs
@@ -31,40 +31,42 @@ public class MapLoader : MonoBehaviour
         string path = ".";//Application.persistentDataPath;
         string[] parts;
 
+        //load default icons - no errors are allowed
         StreamReader def = new StreamReader(path + "/Assets/Resources/Campaigns/default/000_config.txt");
-        foreach (string part in def.ReadToEnd().Split('\n'))
-        {
-            if (part.Contains(":"))
-            {
-                parts = part.Split(':');
-                Debug.Log(((byte)parts[1][parts[1].Length - 1]));
-                icons.Add(parts[0], "Campaigns/default/icons/" + parts[1].Replace("\r", ""));
-            }
-        }
+        parts = def.ReadToEnd().Split('#');
+        loadIcons(parts[0], true);
         def.Close();
 
+        //try to load custom campaign settings
         try
         {
             StreamReader camp = new StreamReader(path + "/Assets/Resources/Campaigns/" + campaign + "/000_config.txt");
-            string[] fileStr = camp.ReadToEnd().Split('#');
+            parts = camp.ReadToEnd().Split('#');
+            loadIcons(parts[0]);
             camp.Close();
-
-            foreach (string part in fileStr[0].Split('\n'))
-            {
-                if (!part.StartsWith("---") && part.Contains(":"))
-                {
-                    parts = part.Split(':');
-                    if (icons.ContainsKey(parts[0]))
-                    {
-                        icons.Remove(parts[0]);
-                        icons.Add(parts[0], "Campaigns/" + campaign + "/icons/" + parts[1].Replace("\r", ""));
-                        Debug.Log(icons[parts[0]]);
-                    }
-                }
-            }
         } catch(IOException e)
         {
             Debug.LogError(e);
+        }
+    }
+
+    void loadIcons(string data, bool allowNewKeys = false)
+    {
+        //for each line assign key and value in dictionary
+        string[] parts;
+        foreach (string part in data.Split('\n'))
+        {
+            if (!part.StartsWith("---") && part.Contains(":"))
+            {
+                parts = part.Split(':');
+                if (icons.ContainsKey(parts[0]) || allowNewKeys)
+                {
+                    //overwrite old value asignment with new assignment
+                    icons.Remove(parts[0]);
+                    icons.Add(parts[0], "Campaigns/" + (allowNewKeys? "default" : campaign) + "/icons/" + parts[1].Replace("\r", ""));
+                    Debug.Log(icons[parts[0]]);
+                }
+            }
         }
     }
 }
