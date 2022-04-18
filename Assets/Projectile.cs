05a2876180292794d09d9ab1c3bb3cd4f4756f02
Laurin <60652077+Flexla54@users.noreply.github.com>
2022-04-18 02:03:10 +0200

    Added Collision (RB), fire-trail, death Explosion

--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -3,21 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+//TO DO: implement kill tank, 
 public class Projectile : MonoBehaviour
 {
+    public int team = 0;
+
     private BoxCollider2D myCollider;
-    private float traveledDistance;
+    private Rigidbody2D rb;
+    public ParticleSystem projTrail;
+    public ParticleSystem explosionSm;
     private int bounceCounter;
     private float speed;
-    private char travelType;
-    private Vector2 movement;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         myCollider = GetComponent<BoxCollider2D>();
-        traveledDistance = 0;
         this.transform.Translate(Vector2.right);
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * speed;
+        projTrail.Play();
+        explosionSm.Pause();
     }
 
     // Update is called once per frame
@@ -25,20 +32,62 @@ public class Projectile : MonoBehaviour
     {
     }
 
-    void FixedUpdate(){
-        MoveProjectile();
+    void FixedUpdate()
+    {
+        
     }
 
-    public void SetParameters(int bounces, float velocity, char flyingType, Quaternion direction){
+    public void SetParameters(int bounces, float velocity, Quaternion direction)
+    {
         this.bounceCounter = bounces;
         this.speed = velocity;
-        this.travelType = flyingType;
         this.transform.rotation = direction;
     }
     
-    void MoveProjectile(){
-        //move the projectile form our postition to the given direction (with our defined speed per time/frame)
-        transform.Translate(Vector2.right * speed * Time.fixedDeltaTime);
-    }
     
+    void OnCollisionEnter2D(Collision2D hit)
+    {
+        //
+        if(hit.gameObject.name.Equals("wall")){
+            TurnProjectile(hit.GetContact(0).normal);
+            bounceCounter = bounceCounter - 1;
+        }
+        else if(hit.gameObject.name.Equals("tank")){
+            //first merge to get team
+        }
+        else if(hit.gameObject.name.Equals("Projectile(Clone)")){
+            DestroyProjectile();
+        }
+
+        if(bounceCounter == 0){
+            DestroyProjectile();
+        }
+    }
+
+    public void DestroyProjectile() 
+    {
+        //Disable Projectile (=invisible), start explosion, kill proj after explosion
+        this.gameObject.SetActive(false);
+        this.rb.velocity = this.transform.right * 0;
+        explosionSm.Play();
+        Destroy(gameObject, 1.4f);
+    }
+
+    public void TurnProjectile (Vector2 mirrorAxis)
+    {
+        //mirror the direction Vector over the normal Vector of the obsticle
+        Vector2 projDir = Vector2.Reflect(this.transform.right, mirrorAxis);
+
+        //calculate the angle of the new Direction Vector
+        float dirAngle = (180 * Mathf.Atan2(projDir.y, projDir.x) / Mathf.PI);
+
+        if (dirAngle < 0)
+        {
+            dirAngle = 360 + dirAngle;
+        }
+
+        //Set the angle of the new Direction and restore speed
+        this.transform.rotation = Quaternion.Euler(0,0, dirAngle);
+        this.rb.velocity = this.transform.right * speed;
+    }
 }
